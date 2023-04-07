36cee61477836504afa47282b6c2def38037664f
Александр Романов <roman.ulbricht@yandex.ru>
2023-04-07 21:26:29 +0400

    testv1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,6 @@ from rest_framework import (filters, pagination, permissions, status, views,
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import SlugFilter
